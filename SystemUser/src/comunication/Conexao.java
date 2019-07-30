de89e17e8db53412e6046f3dc7f3bcee4fc882eb
Juli Aragão <aragaopintojuli@gmail.com>
2019-07-29 21:02:59 -0300

    iniciando

---
+++ SystemUser/src/comunication/Conexao.java
@@ -0,0 +1,47 @@
+package comunication;
+
+import controladores.ControllerDeTratamento;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexao{
+    private Socket socket;
+    private int porta;
+    private ConectionIO io;
+
+    private final ControllerDeTratamento tratamento;
+    
+    public Conexao(ControllerDeTratamento tratamento){
+        this.tratamento = tratamento;
+    }
+ 
+    public Conexao(ControllerDeTratamento tratamento, String ip, int porta){
+        this.tratamento = tratamento;
+        try {
+            socket = criarSocket(ip, porta);
+            io = new ConectionIO(socket, tratamento);
+        } catch (IOException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+        
+    public ConectionIO getConectionIO(){
+        return io;
+    }
+
+    public Socket getSocket(){
+        return socket;
+
+    }
+    
+     private Socket criarSocket(String host, int porta) throws IOException {
+        Socket sock = new Socket();
+        return new Socket(host,porta);
+    }   
+    
+}
+
+
